2f661185d69a6680b1b1323fa58e71fa5db92ff0
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2026-03-19 11:20:11 +0530

    fixed vercel build errors

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest/client";
-import { processMeetingIntelligence } from "@/lib/inngest/functions/meeting-intelligence";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [processMeetingIntelligence],
-  serveHost: "http://127.0.0.1:3000",
-});
